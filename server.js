97bd99b901cfdfb9df3f08f6f210c03d6cb6d6d6
ak14092003 <arpitdav4444@gmail.com>
2024-09-22 16:47:52 +0530

    commit

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const dotenv = require('dotenv');
+dotenv.config()
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || PORT;
 
 app.use(bodyParser.json());
 
